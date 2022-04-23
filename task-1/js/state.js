1bbe07a4885a37203c2ce64d6256f7270fbfcf3f
AntFlopGame <73836816+AntFlopGame@users.noreply.github.com>
2022-04-23 20:05:22 +0300

    commit 1

---
+++ task-1/js/state.js
@@ -0,0 +1,45 @@
+export let state = {
+	_categories: ['Task', 'Random Thought', 'Idea'],
+	_activeTable: [
+		{
+			id: 1,
+            name: 'Shopping list',
+            created: 'April 20, 2021',
+            category: 'Task',
+            content: 'Tomatoes, bread',
+            dates: ''
+		},
+		{
+            id: 2,
+            name: 'The theory of evolution',
+            created: 'April 27, 2021',
+            category: 'Random Thought',
+            content: 'The evolution..',
+            dates: ''
+        },
+        {
+            id: 3,
+            name: 'New Feature',
+            created: 'May 05, 2021',
+            category: 'Idea',
+            content: 'Implement new..',
+            dates: '03/05/2021, 05/05/2021'
+        },
+        {
+            id: 4,
+            name: 'William Gaddis',
+            created: 'May 07, 2021',
+            category: 'Quote',
+            content: 'Power does not co...',
+            dates: ''
+        },
+        {
+            id: 5,
+            name: 'Books',
+            created: 'May 15, 2021',
+            category: 'Task',
+            content: 'The Learn Startup',
+            dates: ''
+        }
+	]
+}
